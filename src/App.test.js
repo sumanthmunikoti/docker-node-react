0a55200efbadb2ffea2b90af79b92c5eae2b5ff4
sumanthmunikoti <sumanthmunikoti1781989@gmail.com>
2021-02-20 14:05:14 -0500

    Initial Commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
-import { render, screen } from '@testing-library/react';
 import App from './App';
+import ReactDOM from 'react-dom'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+it('renders learn react link', () => {
+  const div = document.createElement('div')
+  ReactDOM.render(<App/>, div)
+  ReactDOM.unmountComponentAtNode(div)
+});
+
+it('reners learn react link', () => {
+  const div = document.createElement('div')
+  ReactDOM.render(<App/>, div)
+  ReactDOM.unmountComponentAtNode(div)
 });
